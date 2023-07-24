90d567661e53c4a68feef4020a4609b50930a5fe
nthuy <thuynguyen.webdev@gmail.com>
2023-07-24 22:50:28 +0700

    re-add server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const app = express();
+
+const salt = bcrypt.genSaltSync(10);
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://nthuy650:pV75qtwmT2uT2xSW@blogcluster.wmnacrx.mongodb.net/blog?retryWrites=true&w=majority"
+);
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
+
+app.listen(4000);
